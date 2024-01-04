ca89e79cde94aa684f346f7e0ee2d55acf141d2c
jason.liao <xtulnx@126.com>
2024-01-04 15:32:06 +0800

    feat: gorm 的辅助工具

---
+++ jgorm/debug.go
@@ -0,0 +1,54 @@
+package jgorm
+
+import (
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/utils"
+	"path"
+	"strings"
+)
+
+//go:linkname gormSourceDir gorm.io/gorm/utils.gormSourceDir
+var gormSourceDir string
+
+func DebugGorm() {
+	_ = utils.FileWithLineNum()
+	//fmt.Println("sourceDir", gormSourceDir)
+	if gormSourceDir != "" {
+		gormSourceDir = path.Dir(path.Dir(gormSourceDir))
+		//fmt.Println("targetDir", gormSourceDir)
+	}
+}
+
+// SafeStr 限定字符串长度，避免字段溢出
+func SafeStr(s string, size int) string {
+	if size <= 1 || len(s) < size {
+		return s
+	}
+	cc := []rune(s)
+	if size <= 1 || len(cc) < size {
+		return s
+	}
+	return string(cc[:size])
+}
+
+// SetDbLogger 打开调试日志
+func SetDbLogger(db *gorm.DB, logSql string) *gorm.DB {
+	if logSql == "1" || logSql == "true" || logSql == "info" {
+		db = db.Debug()
+	} else {
+		var t logger.LogLevel
+		switch strings.ToLower(logSql) {
+		case "debug", "info":
+			t = logger.Info
+		case "warn", "warning":
+			t = logger.Warn
+		case "error":
+			t = logger.Error
+		}
+		if t > 0 {
+			db = db.Session(&gorm.Session{Logger: db.Logger.LogMode(t)})
+		}
+	}
+	return db
+}
