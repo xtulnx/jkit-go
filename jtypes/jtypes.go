606e8f4051efff76a5baa03ddf6bc1dcf9681d2a
jason.liao <xtulnx@126.com>
2024-07-13 10:20:35 +0800

    feat: 自定义数据类型

--- jtypes/jtypes.go
+++ jtypes/jtypes.go
@@ -2,11 +2,42 @@ package jtypes
 
 import (
 	"bytes"
+	"math"
 	"strconv"
 )
 
-// 价格，或财务金额，精度2位小数
+// 价格，或财务金额，精度4位小数
 type JPrice float64
+
+func NewPrice(p float64) JPrice {
+	return JPrice(p)
+}
+
+func NewPriceFromCent(cent int64) JPrice {
+	return JPrice(float64(cent) / 100)
+}
+
+func (p JPrice) ToCent() int64 {
+	return int64(math.Round(float64(p) * 100))
+}
+
+func NewPriceFromFixed(fix int64) JPrice {
+	return JPrice(float64(fix) / 1e4)
+}
+
+func (p JPrice) ToFixed() int64 {
+	return int64(math.Round(float64(p) * 1e4))
+}
+
+func (p JPrice) Round() JPrice {
+	return JPrice(math.Round(float64(p)*1e4) / 1e4)
+}
+
+func NewPriceFromString(s string) (JPrice, error) {
+	var p JPrice
+	err := p.UnmarshalText([]byte(s))
+	return p, err
+}
 
 func (p JPrice) Float() float64 {
 	return float64(p)
@@ -16,14 +47,14 @@ const JPriceSize = 2
 
 func (p JPrice) String() string {
 	b1 := strconv.AppendFloat(nil, float64(p), 'f', JPriceSize, 64)
-	if JPriceSize > 0 {
-		l, i := len(b1), 1
-		for i <= JPriceSize && b1[l-i] == '0' {
-			i++
-		}
-		if i > JPriceSize && b1[l-i] == '.' {
-			i++
-		}
+	l, i := len(b1), 1
+	for i <= JPriceSize && b1[l-i] == '0' {
+		i++
+	}
+	if i > JPriceSize && b1[l-i] == '.' {
+		i++
+	}
+	if i > 1 {
 		b1 = b1[:l-i+1]
 	}
 	return string(b1)
@@ -42,6 +73,21 @@ func (dp *JPrice) UnmarshalText(data []byte) error {
 	if len(data) == 0 {
 		return nil
 	}
+
+	top := 0
+	for i, c := range data {
+		switch c {
+		case ' ', '\t', ',':
+			continue
+		default:
+			if top != i {
+				data[top] = data[i]
+			}
+			top++
+		}
+	}
+	data = data[:top]
+
 	t1, err := strconv.ParseFloat(string(data), 64)
 	if err != nil {
 		return err
@@ -57,4 +103,22 @@ func (dp *JPrice) UnmarshalJSON(b []byte) (err error) {
 func (p JPrice) MarshalJSON() ([]byte, error) {
 	s1 := p.String()
 	return []byte(s1), nil
+}
+
+func JPriceEqual(p1, p2 float64) bool {
+	return JPrice(p1).Equal(JPrice(p2))
+}
+
+func JPriceNotEqual(p1, p2 float64) bool {
+	return JPrice(p1).NotEqual(JPrice(p2))
+}
+
+// Equal 相等
+func (p JPrice) Equal(p2 JPrice) bool {
+	return p.ToCent() == p2.ToCent()
+}
+
+// NotEqual 不相等
+func (p JPrice) NotEqual(p2 JPrice) bool {
+	return !p.Equal(p2)
 }
--- jtypes/jtypes_test.go
+++ jtypes/jtypes_test.go
@@ -2,6 +2,8 @@ package jtypes
 
 import (
 	"encoding/json"
+	"fmt"
+	"math"
 	"testing"
 )
 
@@ -39,5 +41,61 @@ func typesPrice(t *testing.T) {
 	for _, i := range data {
 		b1, _ := json.Marshal(i)
 		t.Log(i, "=> FMT:", JPrice(i).String(), ", JSON:", string(b1))
+	}
+}
+
+func TestPrice2(t *testing.T) {
+	fnS := func(s string) JPrice {
+		p, _ := NewPriceFromString(s)
+		return p
+	}
+	fnStr := func(v JPrice) string {
+		return fmt.Sprintf("float=%v, str=%s, cent=%v, fixed=%v, round=%v", v.Float(), v.String(), v.ToCent(), v.ToFixed(), v.Round().Float())
+	}
+	fnEq := func(inc JPrice, ok bool) func(v JPrice) string {
+		return func(v JPrice) string {
+			a := inc + v
+			e := a.Equal(v)
+			if e != ok {
+				t.Errorf("v=%v, a=%v, a.Equal(v)=%v, want %v", v, a, e, ok)
+			}
+			return fmt.Sprintf("%s.Equal(%s) => %v", v.String(), a.String(), e)
+		}
+	}
+	for _, v := range []struct {
+		N    string
+		P    JPrice
+		F    func(v JPrice) string
+		Want string
+	}{
+		{"1/3", 1.0 / 3, fnStr, "float=0.3333333333333333, str=0.33, cent=33, fixed=3333, round=0.3333"},
+		{"2/3", 2.0 / 3, fnStr, "float=0.6666666666666666, str=0.67, cent=67, fixed=6667, round=0.6667"},
+		{"2024/31", 2024.0 / 31, fnStr, "float=65.29032258064517, str=65.29, cent=6529, fixed=652903, round=65.2903"},
+		{"-1/3", -1.0 / 3, fnStr, "float=-0.3333333333333333, str=-0.33, cent=-33, fixed=-3333, round=-0.3333"},
+		{"-2/3", -2.0 / 3, fnStr, "float=-0.6666666666666666, str=-0.67, cent=-67, fixed=-6667, round=-0.6667"},
+		{"2/3-0.002", 2.0/3 - 0.002, fnStr, "float=0.6646666666666666, str=0.66, cent=66, fixed=6647, round=0.6647"},
+		{"2/3-0.0002", 2.0/3 - 0.0002, fnStr, "float=0.6664666666666667, str=0.67, cent=67, fixed=6665, round=0.6665"},
+		{"Nan", fnS("Nan"), fnStr, "float=NaN, str=NaN, cent=-9223372036854775808, fixed=-9223372036854775808, round=NaN"},
+		{"678,901 234,567.890123456", fnS("678,901 234,567.890123456"), fnStr, "float=6.789012345678901e+11, str=678901234567.89, cent=67890123456789, fixed=6789012345678901, round=6.789012345678901e+11"},
+		{"2/3 - 0.002 notEqual", 2.0 / 3, fnEq(-0.02, false), "0.67.Equal(0.65) => false"},
+		{"2/3 - 0.002 notEqual", 2.0 / 3, fnEq(-0.002, false), "0.67.Equal(0.66) => false"},
+		{"2/3 - 0.0002 equal", 2.0 / 3, fnEq(-0.0002, true), "0.67.Equal(0.67) => true"},
+		{"2/3 - 0.00002 equal", 2.0 / 3, fnEq(-0.00002, true), "0.67.Equal(0.67) => true"},
+		{"1234567890 - 7890 notEqual", 1234567890, fnEq(-7890, false), "1234567890.Equal(1234560000) => false"},
+		{"pi*1e20", math.Pi * 1e8, fnStr, "float=3.1415926535897934e+08, str=314159265.36, cent=31415926536, fixed=3141592653590, round=3.14159265359e+08"},
+		// 9,223,372,036,854,775,807
+		{"max cent", NewPriceFromCent(1<<63 - 1), fnStr, "float=9.223372036854776e+16, str=92233720368547760, cent=-9223372036854775808, fixed=-9223372036854775808, round=9.223372036854776e+16"},
+		{"max fixed", NewPriceFromFixed(1<<63 - 1), fnStr, "float=9.223372036854776e+14, str=922337203685477.62, cent=92233720368547760, fixed=-9223372036854775808, round=9.223372036854776e+14"},
+		// 4,611,686,018,427,387,903
+		{"cent62", NewPriceFromCent(1<<62 - 1), fnStr, "float=4.611686018427388e+16, str=46116860184273880, cent=4611686018427387904, fixed=-9223372036854775808, round=4.611686018427388e+16"},
+		{"fixed62", NewPriceFromFixed(1<<62 - 1), fnStr, "float=4.611686018427388e+14, str=461168601842738.81, cent=46116860184273880, fixed=4611686018427387904, round=4.611686018427388e+14"},
+	} {
+		v1 := v
+		t.Run(v.N, func(t *testing.T) {
+			s1 := v1.F(v1.P)
+			if s1 != v1.Want {
+				t.Errorf("%s => %#v, [%s], want [%s]", v1.N, v1.P, s1, v1.Want)
+			}
+		})
 	}
 }
